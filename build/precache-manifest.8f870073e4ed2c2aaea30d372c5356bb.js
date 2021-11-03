e348b26616a5034bd4726ede330a8931788e832e
shen-zilin <1994558470@qq.com>
2021-11-03 21:27:14 +0800

    add build

---
+++ build/precache-manifest.8f870073e4ed2c2aaea30d372c5356bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3a134ac9e602d5aaaf3e",
+    "url": "/My-WebPage/static/css/main.643ec1ab.chunk.css"
+  },
+  {
+    "revision": "3a134ac9e602d5aaaf3e",
+    "url": "/My-WebPage/static/js/main.5de86018.chunk.js"
+  },
+  {
+    "revision": "4a41bc74a4a6614dcdec",
+    "url": "/My-WebPage/static/js/runtime~main.db156bef.js"
+  },
+  {
+    "revision": "9d30762d38ff8bcf8a9b",
+    "url": "/My-WebPage/static/js/2.9bc1ee71.chunk.js"
+  },
+  {
+    "revision": "d36f6c1cb777d66ae922099d7b847aec",
+    "url": "/My-WebPage/static/media/avatar.d36f6c1c.jpg"
+  },
+  {
+    "revision": "89a7810f98b6c8d3e6ef088309689839",
+    "url": "/My-WebPage/index.html"
+  }
+];
